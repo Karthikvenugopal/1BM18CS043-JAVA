33ed5e53c300c5e24d78bb1d239a45a8654a786f
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2019-08-26 17:12:52 +0530

    Add files via upload

---
+++ SGPA.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SGPA
+{
+    public static void main(String args[])
+    {
+        Student s = new Student();
+        s.input();
+        s.cal();
+        s.output();
+    }
+}
