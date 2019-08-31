ba13119a7387271ad45d79cd54f1696890288874
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2019-08-31 20:41:17 +0530

    Update Points.java

--- Points.java
+++ Points.java
@@ -31,3 +31,20 @@ public class Points
         System.out.println("The coordinates of the point is ("+x+", "+y+")");
     }
 }
+public class Points_demo
+{
+    public static void main(String args[])
+    {
+        Points p1 = new Points();
+        System.out.println("Enter the coordinates of second the point");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Points p2 = new Points(a,b);
+        double k = p1.dist(p2);
+        Points p3 = new Points(p1);
+        p3.display();
+        p2.display();
+        System.out.println("Distance between the two above points = " + k);
+    }
+}
