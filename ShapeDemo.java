9de15ccdd2fa7f520a3f6ac5854d9a7cf62d509b
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2019-09-27 14:35:06 +0530

    Add files via upload

---
+++ ShapeDemo.java
@@ -0,0 +1,62 @@
+import java.util.*;
+abstract class Shape
+{
+   int l,b;
+   abstract void printArea();
+}
+class Rectangle extends Shape
+{
+   void printArea()
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the length and breadth");
+      l = sc.nextInt();
+      b = sc.nextInt();
+      System.out.println("Area = " + l*b);
+   }
+}
+class Triangle extends Shape
+{
+   void printArea()
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the length and height");
+      l = sc.nextInt();
+      b = sc.nextInt();
+      System.out.println("Area = " + ((l*b)/2.0));
+   }
+}
+class Circle extends Shape
+{
+   void printArea()
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the radius");
+      l = sc.nextInt();
+      b = 0;
+      System.out.println("Area = " + (3.14*l*l));
+   }
+}
+class ShapeDemo
+{
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter 1 for rectangle");
+      System.out.println("Enter 2 for triangle");
+      System.out.println("Enter 3 for circle");
+      int c = sc.nextInt();
+      switch(c)
+      {
+         case 1: Rectangle r = new Rectangle();
+                 r.printArea();
+                 break;
+         case 2: Triangle t = new Triangle();
+                 t.printArea();
+                 break;
+         case 3: Circle k = new Circle();
+                 k.printArea();
+                 break;
+      }
+  }
+}        
